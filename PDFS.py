c466055fe2cc2ce7d2afe5b006b933288dbc0bf1
wouterprommel <wouterprommel@gmail.com>
2024-04-23 20:41:44 +0200

    fuck git

--- PDFS.py
+++ PDFS.py
@@ -10,16 +10,16 @@ def GeV_to_pb(cs):
 class mc_cross_section():
 
     def __init__(self, E_nu, pdf, regions, n_samples_list):
-        self.Mn = 0.938
+        Mn = 0.938
         self.pdf = pdf
 
         self.GF = 1.663787e-5
         self.Mw = 80.385
-        self.s = 2*E_nu*self.Mn
+        self.s = 2*E_nu*Mn
 
         self.cs = 0.0
         #print(f's: {self.s}, Q2_max: {self.pdf.q2Max}, diff s q2max: {self.pdf.q2Max - self.s}')
-        #print(f'pdf ranges: x min: {pdf.xMin}, x max:{pdf.xMax}, Q2 min:{pdf.q2Min}, Q2 max{pdf.q2Max}')
+        print(f'pdf ranges: x min: {pdf.xMin}, x max:{pdf.xMax}, Q2 min:{pdf.q2Min}, Q2 max{pdf.q2Max}')
         assert self.s < self.pdf.q2Max, 'E_nu too high, s > q2max'
         assert len(regions) - 1 == len(n_samples_list), 'regions must be 1 longer than n_samples'
         n_samples_list = [0] + n_samples_list
@@ -104,7 +104,7 @@ class mc_cross_section():
 
     def calc(self, X, Q2, A):
         integral = self._mc(X, Q2, A)
-        return (self.GF*self.GF * self.Mw**4 * integral)/(2*np.pi) # factor 2 from (N + P)/2   But just 4 for the structure functions bc those are multiplied by 2...
+        return (self.GF*self.GF * self.Mw**4)/(4*np.pi) * integral # factor 2 from (N + P)/2 extra 2 factor from anti-neutrino But just 4 for the structure functions bc those are multiplied by 2...
 
     def calc_vis(self):
         integral, int_list = self._mc_list()
@@ -129,8 +129,8 @@ class mc_cross_section():
         integral3 = 0.0
         n_samples = len(X)
         for i in range(n_samples):
-            if False and i % 1000000 == 0:
-                print(i)
+            if i % 1000000 == 0:
+                 print(i)
             integral += self._diff_cs_neutrino_nuclei_struc_func(X[i], Q2[i])
             integral2 += self._diff_cs_nn(X[i], Q2[i])
             integral3 += self._diff_cs_neutrino_nuclei(X[i], Q2[i])
@@ -140,9 +140,9 @@ class mc_cross_section():
         integral *= A / n_samples 
         integral2 *= A / n_samples 
         integral3 *= A / n_samples 
-        #print(f'Difference between the two integrals:', integral, integral2, integral3, integral/integral2, integral/integral3)
+        print(f'Difference between the two integrals:', integral, integral2, integral3, integral/integral2, integral/integral3)
         
-        return integral3
+        return integral2
 
     def _mc_list(self):
         n_samples = len(self.x_region)
@@ -163,9 +163,12 @@ class mc_cross_section():
         assert Q2 < self.pdf.q2Max, f'Q2 out of range {Q2 - self.pdf.q2Max}'
         a = 1/(self.Mw*self.Mw + Q2)**2
         b = (self.pdf.xfxQ2(1, x, Q2) + self.pdf.xfxQ2(2, x, Q2) + 2*self.pdf.xfxQ2(3, x, Q2))/(x)
+        anti_b = (self.pdf.xfxQ2(-1, x, Q2) + self.pdf.xfxQ2(-2, x, Q2) + 2*self.pdf.xfxQ2(-3, x, Q2))/(x)
         c = (1 - Q2/(x*self.s))**2
+        assert c > 0
         d = (self.pdf.xfxQ2(-1, x, Q2) + self.pdf.xfxQ2(-2, x, Q2) + 2*self.pdf.xfxQ2(-4, x, Q2))/(x)
-        return a*((b) + c*(d)) 
+        anti_d = (self.pdf.xfxQ2(1, x, Q2) + self.pdf.xfxQ2(2, x, Q2) + 2*self.pdf.xfxQ2(4, x, Q2))/(x)
+        return 4*a*((b) + c*(d)) # should be times 2 i think.. because where not using anti-neutrino
 
     def _diff_cs_neutrino_nuclei_struc_func(self, x, Q2):
         a = 1/(self.Mw*self.Mw + Q2)**2
@@ -173,13 +176,13 @@ class mc_cross_section():
         Ym = 1 - (1 - Q2/(x*self.s))**2
         F2 = 2*(self.pdf.xfxQ2(1, x, Q2) + self.pdf.xfxQ2(2, x, Q2) + self.pdf.xfxQ2(-1, x, Q2) + self.pdf.xfxQ2(-2, x, Q2) + 2*self.pdf.xfxQ2(3, x, Q2) + 2*self.pdf.xfxQ2(-4, x, Q2))
         xF3 = 2*((self.pdf.xfxQ2(2, x, Q2) - self.pdf.xfxQ2(-2, x, Q2)) + (self.pdf.xfxQ2(1, x, Q2) - self.pdf.xfxQ2(-1, x, Q2)) + 2*self.pdf.xfxQ2(3, x, Q2) - 2*self.pdf.xfxQ2(-4, x, Q2))
-        return a*(Yp*F2 + Ym*xF3)/(2*x)
+        return a*(Yp*F2 + Ym*xF3)/x
 
     def _diff_cs_nn(self, x, Q2):
         a = 2/(self.Mw*self.Mw + Q2)**2
-        b = (2*self.pdf.xfxQ2(2, x, Q2) + 2*self.pdf.xfxQ2(1, x, Q2) + 4*self.pdf.xfxQ2(3, x, Q2))/(2*x)
+        b = (2*self.pdf.xfxQ2(2, x, Q2) + 2*self.pdf.xfxQ2(1, x, Q2) + 4*self.pdf.xfxQ2(3, x, Q2))/x
         c = (1 - Q2/(x*self.s))**2
-        d = (2*self.pdf.xfxQ2(-2, x, Q2) + 2*self.pdf.xfxQ2(-1, x, Q2) + 4*self.pdf.xfxQ2(-4, x, Q2))/(2*x)
+        d = (2*self.pdf.xfxQ2(-2, x, Q2) + 2*self.pdf.xfxQ2(-1, x, Q2) + 4*self.pdf.xfxQ2(-4, x, Q2))/x
         return a*(b + c*d)
 
 df = pd.read_csv('cs.csv')
@@ -194,18 +197,13 @@ regions = [0, 1e-3, 1e-2, 5e-2, 1e-1, 0.2, 1]
 n_samples = [2e7, 2e6, 2e6, 2e6, 2e6, 2e6]
 if False:
     E_nu = 1e6
-    regions = [0, 1e-3, 1e-2, 5e-2, 1e-1, 0.2, 1]
-    n_samples_list = [2e6, 2e6, 2e6, 2e6, 2e6, 2e6]
-    goal = 628
-if False:
-    E_nu = 1e8
-    regions = [0, 1e-5, 1e-4, 5e-4, 1e-3, 1e-2, 0.1, 1]
-    n_samples_list = [1e6, 2e5, 2e5, 2e5, 2e5, 2e5, 2e5]
-    goal = 4250
+    regions = [0, 1e-3, 1e-2, 1e-1, 0.2, 1]
+    n_samples_list = [2e5, 2e5, 2e5, 2e5, 2e5]
 if False:
     E_nu = 1e4
     regions = [0, 1e-3, 1e-2, 1e-1, 0.2, 1]
     n_samples_list = [ 1e5, 1e5, 1e5, 1e4, 1e4]
+<<<<<<< HEAD
     goal = 44.6
 for i in range(16):
     E_nu = df.at[i, 'E_nu']
@@ -222,3 +220,10 @@ for i in range(16):
     df.to_csv('cs.csv', index=False)
 
 # new change
+=======
+for i in range(1):
+    mc_cs = mc_cross_section(E_nu, pdf, regions, n_samples_list)
+
+    print("Cross-Section Neutrino-Proton:", GeV_to_pb(mc_cs.cs) , 'pb, at E_nu: 1e', len(str(E_nu)) - 3, 'GeV\n\n')
+
+>>>>>>> parent of 7a51b33 (factors of 2 are urgh)
