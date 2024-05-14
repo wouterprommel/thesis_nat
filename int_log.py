743749e5962402e11a722a2037e93e65d8f1e133
Wouter Prommel <wouterprommel@gmail.com>
2024-05-14 22:14:47 +0200

    neutron and proton seperate calc

--- int_log.py
+++ int_log.py
@@ -23,6 +23,8 @@ class cs_neutrino_nucleon:
         self.conv_p = 389379290.4730569
         self.conv = 0.3894e9
 
+        self.target = 'isoscalar'
+
         self.calc_count = 0
         if self.s < self.pdf.q2Max:
            self.physical = True 
@@ -97,27 +99,36 @@ class cs_neutrino_nucleon:
         Yp = 1 + omy2
         Ym = 1 - omy2
         fact = self.conv * self.GF2 / 4 / np.pi * np.power(self.Mw2 / (self.Mw2 + Q2 ), 2)
-        #print('fact', fact, 'conf', self.conv, 'pow', np.power(self.Mw2 / (self.Mw2 + Q2 ), 2), 'GF2', self.GF2)
-        #print('Yp', Yp)
-        #quit()
-        # was 2*() for f2 and f3
-        F2 = (self.pdf.xfxQ2(1, x, Q2) + self.pdf.xfxQ2(2, x, Q2) + self.pdf.xfxQ2(-1, x, Q2) + self.pdf.xfxQ2(-2, x, Q2) + 2*self.pdf.xfxQ2(3, x, Q2) + 2*self.pdf.xfxQ2(-4, x, Q2))
-        xF3 = ((self.pdf.xfxQ2(2, x, Q2) - self.pdf.xfxQ2(-2, x, Q2)) + (self.pdf.xfxQ2(1, x, Q2) - self.pdf.xfxQ2(-1, x, Q2)) + 2*self.pdf.xfxQ2(3, x, Q2) - 2*self.pdf.xfxQ2(-4, x, Q2))
+
+        if self.target == 'isoscaler':
+            F2 = (self.pdf.xfxQ2(1, x, Q2) + self.pdf.xfxQ2(2, x, Q2) + self.pdf.xfxQ2(-1, x, Q2) + self.pdf.xfxQ2(-2, x, Q2) + 2*self.pdf.xfxQ2(3, x, Q2) + 2*self.pdf.xfxQ2(-4, x, Q2))
+            xF3 = ((self.pdf.xfxQ2(2, x, Q2) - self.pdf.xfxQ2(-2, x, Q2)) + (self.pdf.xfxQ2(1, x, Q2) - self.pdf.xfxQ2(-1, x, Q2)) + 2*self.pdf.xfxQ2(3, x, Q2) - 2*self.pdf.xfxQ2(-4, x, Q2))
+
+        elif self.target == 'proton':
+            F2 = 2*(self.pdf.xfxQ2(1, x, Q2) + self.pdf.xfxQ2(-2, x, Q2) + self.pdf.xfxQ2(3, x, Q2) + self.pdf.xfxQ2(-4, x, Q2))
+            xF3 = 2*(self.pdf.xfxQ2(1, x, Q2) - self.pdf.xfxQ2(-2, x, Q2) + self.pdf.xfxQ2(3, x, Q2) - self.pdf.xfxQ2(-4, x, Q2))
+    
+        elif self.target == 'neutron':
+            F2 = 2*(self.pdf.xfxQ2(-1, x, Q2) + self.pdf.xfxQ2(2, x, Q2) + self.pdf.xfxQ2(3, x, Q2) + self.pdf.xfxQ2(-4, x, Q2))
+            xF3 = 2*(self.pdf.xfxQ2(-1, x, Q2) + self.pdf.xfxQ2(2, x, Q2) + self.pdf.xfxQ2(3, x, Q2) - self.pdf.xfxQ2(-4, x, Q2))
+
+
+        # NLO
         #return fact * (Yp * self.pdf.xfxQ2(2001, x, Q2) - y*y * self.pdf.xfxQ2(2002, x, Q2) + Ym * self.pdf.xfxQ2(2003, x, Q2))
         return fact * (Yp * F2 + Ym * xF3)
 
 #pdf_struc = lhapdf.mkPDF("NNPDF31sx_nnlonllx_as_0118_LHCb_nf_6_SF")
 pdf_31 = lhapdf.mkPDF("NNPDF31_lo_as_0118")
-pdf_40 = lhapdf.mkPDF("NNPDF40_lo_as_01180")
-pdf_21 = lhapdf.mkPDF("NNPDF21_lo_as_0119_100")
+#pdf_40 = lhapdf.mkPDF("NNPDF40_lo_as_01180")
+#pdf_21 = lhapdf.mkPDF("NNPDF21_lo_as_0119_100")
 #cs = cs_neutrino_nucleon(1e6, pdf)
 
 df = pd.read_csv('cs_3.csv')
-df['log40_err'] = 19*[0.0]
-df['log31_err'] = 19*[0.0]
-df['log21_err'] = 19*[0.0]
+name = 'neutron_pdf31'
+df[name] = 19*[0.0]
+df[name + '_err'] = 19*[0.0]
 
-for name, pdf in [('log21', pdf_21), ('log31', pdf_31), ('log40', pdf_40)]:
+for name, pdf in [(name, pdf_31)]:#, ('log40', pdf_40), ('log21', pdf_21)]:
 #for name, pdf in [('log40', pdf_40)]:
 # 0, 19 all 
 # 7, 8 for 1e6
@@ -125,6 +136,7 @@ for name, pdf in [('log21', pdf_21), ('log31', pdf_31), ('log40', pdf_40)]:
         E_nu = df.at[i, 'E_nu']
         dt_start = datetime.datetime.now()
         cs = cs_neutrino_nucleon(E_nu, pdf)
+        cs.target = 'neutron'
         print('physical', cs.physical)
         if cs.physical:
             sigma, err = cs.calc()
