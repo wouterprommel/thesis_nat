58b37453f72a52bcfb3665292d1de1d2ae4dc187
wouterprommel <wouterprommel@gmail.com>
2024-05-28 16:24:51 +0200

    NLO (not working)

--- int_log.py
+++ int_log.py
@@ -118,32 +118,72 @@ class cs_neutrino_nucleon:
                 F2 = 2*(self.pdf.xfxQ2(1, x, Q2) + self.pdf.xfxQ2(-2, x, Q2) + self.pdf.xfxQ2(-3, x, Q2) + self.pdf.xfxQ2(4, x, Q2))
                 xF3 = 2*(+self.pdf.xfxQ2(1, x, Q2) - self.pdf.xfxQ2(-2, x, Q2) - self.pdf.xfxQ2(-3, x, Q2) + self.pdf.xfxQ2(4, x, Q2))
         return F2, xF3
-    def f(i, x, Q2):
-        if i == 1:
 
+    def Cqiz(self, z):
+        '''Coefficient function for the quarks'''
+        epsilon = 0.05
+        a = (1 + z*z) * (np.log(1 - z)/(1 - z))
+        b = - np.divide((1 + z*z), (1 - z)) * np.log(z)
+        c = -3/2 / (1 - z) + 3 + 2*z
+        if z <= 1 + epsilon and z >= 1 - epsilon:
+            d = -(9/2 + np.pi*np.pi/3)
+        else:
+            d= 0.0
 
+        return a + b + c + d -2*z
+        #elif i == 2:
+        #    return a + b + c + d 
+        #elif i == 3:
+        #    return a + b + c + d - (1 + z) 
+    def Cg(self, z):
+        a = np.power((1 - z), 2) + z*z
+        b = np.log((1 - z) / z)
+        c = 6*z*(1 - z)
+        return a*b + c + 4*z*(1 - z)
+    
+    def convolution(self, C, q, x, Q2):
+        conv_int = lambda  z: C(x/z) * self.pdf.xfxQ2(q, z, Q2) / z
+        conv, err = integrate.quad(conv_int, x, 0.999)
+        return conv
 
+    def struc_NLO(self, x, Q2):
+        quarks = ['up', 'down', 'strange', 'charm']
+        flavours = {'up':2, 'down':1, 'strange':3, 'charm':4,}
+        anti_flavours = {'up':-2, 'down':-1, 'strange':-3, 'charm':-4}
+        K1 = {'up':1/4, 'down':1/4, 'strange':1/4, 'charm':1/4,}
+        K2 = {'up':1/2, 'down':1/2, 'strange':1/2, 'charm':1/2,}
 
-        elif i == 2:
-            pass 
-        elif i == 3:
-            pass 
-        return f
+        print(f'alpha Q2:{self.pdf.alphasQ2(Q2)}, \nconvolution quark down: {self.convolution(self.Cqiz, flavours["down"], x, Q2)}, \nconvolution gluon: {self.convolution(self.Cg, 21, x, Q2)}')
+        xF1 = np.array([K1[q]*(self.pdf.xfxQ2(flavours[q], x, Q2) + self.pdf.xfxQ2(anti_flavours[q], x, Q2)
+                               + self.pdf.alphasQ2(Q2)*(self.convolution(self.Cqiz, flavours[q], x, Q2) + 
+                                                       self.convolution(self.Cqiz, anti_flavours[q], x, Q2) + 
+                                                       self.convolution(self.Cg, 21, x, Q2))
+                               ) for q in quarks]).sum()
+        F2 = np.array([K2[q]*(self.pdf.xfxQ2(flavours[q], x, Q2) + self.pdf.xfxQ2(anti_flavours[q], x, Q2)) for q in quarks]).sum()
+        xF3 = np.array([K2[q]*(self.pdf.xfxQ2(flavours[q], x, Q2) - self.pdf.xfxQ2(anti_flavours[q], x, Q2)) for q in quarks]).sum()
+        return xF1, F2, xF3
 
-    def NLO(self, x, Q2):
+    def struc_LO(self, x, Q2):
         quarks = ['up', 'down', 'strange', 'charm']
         anti_quarks = ['anti-up', 'anti-down', 'anti-strange', 'anti-charm']
-        #flavours = {'up':2, 'down':1, 'strange':3, 'charm':4,}
+        flavours = {'up':2, 'down':1, 'strange':3, 'charm':4,}
+        anti_flavours = {'up':-2, 'down':-1, 'strange':-3, 'charm':-4}
         #anti_flavours = {'anti-up':-2, 'anti-down':-1, 'anti-strange':-3, 'anti-charm':-4}
         #f1 = {'up':0.0, 'down':0.0, 'strange':0.0, 'charm':0.0,}
         #f1a = {'anti-up':0.0, 'anti-down':0.0, 'anti-strange':0.0, 'anti-charm':0.0}
-        flavours = {'up':2, 'anti-up':-2, 'down':1, 'anti-down':-1, 'strange':3, 'anti-strange':-3, 'charm':4, 'anti-charm':-4}
-        f1 = {'up':0.0, 'anti-up':0.0, 'down':0.0, 'anti-down':0.0, 'strange':0.0, 'anti-strange':0.0, 'charm':0.0, 'anti-charm':0.0}
-        f1 = self.f(1, x, Q2)
-        f1_g = 0
-        g = self.pdf(21, x, Q2)
+        #flavours = {'up':2, 'anti-up':-2, 'down':1, 'anti-down':-1, 'strange':3, 'anti-strange':-3, 'charm':4, 'anti-charm':-4}
+        #f1 = {'up':0.0, 'anti-up':0.0, 'down':0.0, 'anti-down':0.0, 'strange':0.0, 'anti-strange':0.0, 'charm':0.0, 'anti-charm':0.0}
+        #f1 = self.f_LO(1, x, Q2)
+        #f1_g = 0
+        #g = self.pdf(21, x, Q2)
+        K1 = {'up':1/4, 'down':1/4, 'strange':1/4, 'charm':1/4,}
+        K2 = {'up':1/2, 'down':1/2, 'strange':1/2, 'charm':1/2,}
 
-        F1 = [self.convolution(flavours[qi], f1[qi]) for qi in quarks] + [self.convolution(flavours[aqi], f1[aqi]) for aqi in anti_quarks] + self.convolution(g, f1_g)
+        #F1 = [self.convolution(flavours[qi], f1[qi]) for qi in quarks] + [self.convolution(flavours[aqi], f1[aqi]) for aqi in anti_quarks] + self.convolution(g, f1_g)
+        xF1 = np.array([K1[q]*(self.pdf.xfxQ2(flavours[q], x, Q2) + self.pdf.xfxQ2(anti_flavours[q], x, Q2)) for q in quarks]).sum()
+        F2 = np.array([K2[q]*(self.pdf.xfxQ2(flavours[q], x, Q2) + self.pdf.xfxQ2(anti_flavours[q], x, Q2)) for q in quarks]).sum()
+        xF3 = np.array([K2[q]*(self.pdf.xfxQ2(flavours[q], x, Q2) - self.pdf.xfxQ2(anti_flavours[q], x, Q2)) for q in quarks]).sum()
+        return xF1, F2, xF3
 
     def ddiff_lnx_lnQ2(self, lnx, Q2):
         self.calc_count += 1
@@ -152,6 +192,8 @@ class cs_neutrino_nucleon:
         #x = lnx
         #x = 0.1
         #Q2 = 100
+        if self.calc_count > 20:
+            quit()
 
         y = Q2 / x / self.s
         omy2 = np.power((1-y), 2)
@@ -160,8 +202,11 @@ class cs_neutrino_nucleon:
         fact = self.convert * self.GF2 / 4 / np.pi * np.power(self.Mw2 / (self.Mw2 + Q2 ), 2)
 
         if self.NLO:
-            F1, F2, xF3 = self.NLO(x, Q2)
-            return fact * (F1 + F2 + xF3)
+            fact = self.convert * self.GF2 / np.pi * np.power(self.Mw2 / (self.Mw2 + Q2 ), 2) # the paper has extra factor 2
+            xF1, F2, xF3 = self.struc_NLO(x, Q2)
+            xF1_lo, F2_lo, xF3_lo = self.struc_LO(x, Q2)
+            print(f'diff xF1 NLO: {xF1}, xF1 lo: {xF1_lo}, diff: {np.abs(xF1 - xF1_lo)}')
+            return fact * (F2*(1-y) + xF1*y*y + xF3*y*(1 - y/2))
             # NLO from paper
             #return fact * (Yp * self.pdf.xfxQ2(2001, x, Q2) - y*y * self.pdf.xfxQ2(2002, x, Q2) + Ym * self.pdf.xfxQ2(2003, x, Q2))
 
@@ -179,7 +224,7 @@ pdf_31 = lhapdf.mkPDF("NNPDF31_lo_as_0118")
 #cs = cs_neutrino_nucleon(1e6, pdf)
 
 df = pd.read_csv('cs_3.csv')
-name = 'pdf31_DIY'
+name = 'pdf31_NLO'
 df[name] = 19*[0.0]
 df[name + '_err'] = 19*[0.0]
 
@@ -190,7 +235,7 @@ for name, pdf in [(name, pdf_31)]:#, ('log40', pdf_40), ('log21', pdf_21)]:
     for i in range(0, 19): # 19 to end
         E_nu = df.at[i, 'E_nu']
         dt_start = datetime.datetime.now()
-        cs = cs_neutrino_nucleon(E_nu, pdf, anti=False, target='isoscalar')
+        cs = cs_neutrino_nucleon(E_nu, pdf, anti=False, target='isoscalar', NLO=True)
         print('physical', cs.physical)
         if cs.physical:
             sigma, err = cs.calc()
